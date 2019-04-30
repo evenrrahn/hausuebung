5bb1e61ca25bb33214ea7e41f67db7810d3865a3
Rahn, Robert <robert.rahn@eventim.de>
2019-04-30 14:19:23 +0200

    Add Cheese

---
+++ myMicroservice/Cheese.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myMicroservice
+{
+    public class Cheese
+    {
+        public int AmountOfHoles { get; set; }
+        public string Name { get; set; }
+        public CheeseType CheeseType { get; set; }
+
+        public static Cheese CreateGoatCheese(int amountOfHoles, string name)
+        {
+            return new Cheese(amountOfHoles,name,CheeseType.Goat);
+        }
+        public static Cheese CreateCowCheese(int amountOfHoles, string name)
+        {
+            return new Cheese(amountOfHoles, name, CheeseType.Cow);
+        }
+        private Cheese(int amountOfHoles, string name, CheeseType type)
+        {
+            AmountOfHoles = amountOfHoles;
+            Name = name;
+            CheeseType = type;
+        }
+    }
+}
---
+++ myMicroservice/Controllers/CheeseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheeseController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Cheese>> Get()
+        {
+            return new[] { Cheese.CreateCowCheese(6, "Kuhkäse"), Cheese.CreateGoatCheese(0, "Cabrales")};
+        }
+    }
+}
